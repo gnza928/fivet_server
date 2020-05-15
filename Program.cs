10c4cc8fa930839b9d2481238cda44ed80fb4eb9
gnza928 <50747609+gnza928@users.noreply.github.com>
2020-05-15 13:14:06 -0400

    chore: initial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fivet.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
